b5fd6a217bc2c540e4e07fad88232b37b51af723
Pablito AM <soypablitoam@gmail.com>
2020-12-17 20:35:20 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <Navbar />
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Navbar from "./components/ui/Navbar.vue";
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    Navbar,
+  },
+};
+</script>
---
+++ src/components/ui/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="mx-auto bg-gray-100 p-5 shadow">
+    <nav class="flex justify-between">
+      <div>
+        <a href="#">Logo</a>
+      </div>
+      <ul class="flex flex-row">
+        <li class="pr-5"><a> Services </a></li>
+        <li class="pr-5"><a>Porfolio</a></li>
+        <li class="pr-5"><a>About</a></li>
+        <li><a>Contact</a></li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
